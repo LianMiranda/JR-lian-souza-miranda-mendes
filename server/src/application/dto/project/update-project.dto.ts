44286e8cdd06155f0ebc8fa9937391e490265ae8
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-07-26 16:31:13 -0400

    feat: add Dtos to the project

---
+++ server/src/application/dto/project/update-project.dto.ts
@@ -0,0 +1,8 @@
+import { PartialType } from '@nestjs/mapped-types';
+
+class _UpdateProjectDto {
+  name: string;
+  description: string;
+}
+
+export class UpdateProjectDto extends PartialType(_UpdateProjectDto) {}
