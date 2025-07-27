b50787151569b063eed55435a2a1d87c74ab50a9
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-07-26 21:00:13 -0400

    feat: add task module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectModule } from './project/project.module';
 import { DatabaseModule } from './shared/infrastructure/database/database.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [DatabaseModule, ProjectModule],
+  imports: [DatabaseModule, ProjectModule, TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/task/task.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { TaskController } from './presentation/controllers/task.controller';
+import { TaskModel } from 'src/shared/infrastructure/database/models/task.model';
+import { TaskRepository } from './infrastructure/repositories/task.repository';
+import { CreateTaskUseCase } from './application/use-cases/task/create.use-case';
+import { FindAllTasksUseCase } from './application/use-cases/task/findAll.use-case';
+import { FindTaskByIdUseCase } from './application/use-cases/task/findById.use-case';
+import { UpdateTaskUseCase } from './application/use-cases/task/update.use-case';
+import { DeleteTaskUseCase } from './application/use-cases/task/delete.use-case';
+
+@Module({
+  imports: [SequelizeModule.forFeature([TaskModel])],
+  controllers: [TaskController],
+  providers: [
+    TaskRepository,
+    {
+      provide: 'ITaskRepository',
+      useExisting: TaskRepository,
+    },
+    CreateTaskUseCase,
+    FindAllTasksUseCase,
+    FindTaskByIdUseCase,
+    UpdateTaskUseCase,
+    DeleteTaskUseCase,
+  ],
+  exports: ['ITaskRepository'],
+})
+export class TaskModule {}
