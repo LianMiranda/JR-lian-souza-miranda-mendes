134dcc64b9419f3992aca19910d94762ab3739a3
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-07-26 20:36:09 -0400

    feat: add method fromDatabase in task entity

--- server/src/task/domain/entities/task.entity.ts
+++ server/src/task/domain/entities/task.entity.ts
@@ -25,6 +25,20 @@ export class Task {
     this.updated_at = new Date();
   }
 
+  static fromDatabase(dbTask: any): Task {
+    const task = new Task(dbTask.title, dbTask.project_id, dbTask.description);
+
+    task.id = dbTask.id;
+    task.title = dbTask.title;
+    task.description = dbTask.description || '';
+    task.status = dbTask.status;
+    task.project_id = dbTask.project_id;
+    task.created_at = dbTask.created_at;
+    task.updated_at = dbTask.updated_at;
+
+    return task;
+  }
+
   updateTitle(newTitle: string): void {
     this.title = newTitle;
   }
