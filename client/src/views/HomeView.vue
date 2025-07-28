8230a775a8c9f085fcb771bb5fb01495c1b2fbf1
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-07-28 11:05:29 -0400

    feat: initial home view

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
+import Button from '@/components/common/Button.vue';
 import Header from '@/components/ui/Header.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
+function goToRegistration() {
+  router.push('/create/project');
+}
 
 </script>
 
 <template>
-  <main>
+  <main class="container">
     <Header />
+
+    <div class="p-4">
+      <Button variant="primary" @click="goToRegistration">
+          Cadastrar
+      </Button>
+    </div>
+
   </main>
 </template>
