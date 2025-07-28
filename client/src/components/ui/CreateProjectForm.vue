127c425b354e6fac2f3df2de565fb5a810da8a9a
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-07-28 19:25:54 -0400

    feat: saving project in backend

--- client/src/components/ui/CreateProjectForm.vue
+++ client/src/components/ui/CreateProjectForm.vue
@@ -1,26 +1,59 @@
 <script setup lang="ts">
+import axios from 'axios';
 import Button from '../common/Button.vue';
+import { ref } from 'vue';
+import router from '@/router';
+
+const name = ref('');
+const description = ref('')
+const loading = ref(false);
+
+async function createProject() {
+    if (!name.value.trim()) {
+        alert('Por favor, preencha o campo "Nome"');
+        return;
+    }
+
+    loading.value = true;
+    try {
+        await axios.post('/projects', {
+            name: name.value.trim(),
+            description: description.value.trim()
+        });
+
+        router.push("/")
+    } catch (error) {
+        console.error('Erro ao criar projeto:', error);
+    } finally{
+        loading.value = false;
+    }
+}
+
+function handleSubmit(event: Event){
+    event.preventDefault();
+    createProject();
+}
 
 </script>
 
 <template>
-    <form class="flex flex-col bg-dark h-full p-8 gap-5">
+    <form @submit="handleSubmit" class="flex flex-col bg-dark h-full p-8 gap-5">
         <h2 class="text-2xl font-bold text-center text-black">Cadastro</h2>
 
         <div>
             <label class="block mb-1 text-sm font-medium text-black">Nome</label>
-            <input type="text" name="name" placeholder="Nome do projeto"
+            <input v-model="name" type="text" name="name" placeholder="Nome do projeto" required
                 class="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
         </div>
 
         <div>
             <label class="block mb-1 text-sm font-medium text-black">Descrição</label>
-            <textarea name="description" placeholder="Descrição do projeto"
+            <textarea v-model="description" name="description" placeholder="Descrição do projeto"
                 class="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
         </div>
 
-        <Button type="submit" variant="primary">
-            Criar
+        <Button customClass="cursor-pointer" type="submit" variant="primary" :disabled="loading">
+           {{ loading ? 'Criando...' : 'Criar'}}
         </Button>
     </form>
 </template>
