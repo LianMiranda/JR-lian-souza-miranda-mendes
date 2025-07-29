7ac30e1854a56bb34e26decef1746261c15171df
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-07-29 15:29:28 -0400

    refactor: removing the projects link from the header

--- client/src/components/ui/Header.vue
+++ client/src/components/ui/Header.vue
@@ -5,9 +5,6 @@
   <header class="bg-blue-600 text-white py-4 px-6 shadow-md">
     <div class="mx-auto flex justify-between items-center">
       <h1 class="text-xl font-bold"><a href="/">SolarMarket</a></h1>
-      <nav class="flex gap-2">
-        <a href="/" class="hover:underline">Projetos</a>
-      </nav>
     </div> 
   </header>
 </template>
