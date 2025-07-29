f3df804af70d5131946fee4dda7f12cbee2e42f7
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-07-29 15:31:05 -0400

    feat: showing the task description

--- client/src/components/ui/ProjectTasks.vue
+++ client/src/components/ui/ProjectTasks.vue
@@ -57,12 +57,18 @@ onMounted(fetchTasks)
 <template>
     <div class="container flex gap-4 overflow-x-auto">
         <div v-for="(column, index) in columns" :key="index"
-            class="bg-white rounded-2xl p-4 shadow-md w-64 flex-shrink-0">
+            class="bg-white rounded-2xl p-4 shadow-md w-70 flex-shrink-0">
             <h2 class="text-center text-lg font-semibold mb-4">{{ column.title }}</h2>
             <div class="flex flex-col gap-3">
-                <div v-for="(task, tIndex) in column.tasks" :key="tIndex" class="bg-gray-100 p-3 rounded-lg shadow-sm">
-                    {{ task.title }}
-                    <div class="flex justify-end mb-2 gap-2">
+                <div v-for="(task, tIndex) in column.tasks" :key="tIndex" class="flex flex-col gap-2 bg-gray-100 p-3 rounded-lg shadow-sm">
+                    <h3
+                        class="text-xl font-bold text-gray-800 mb-2 group-hover:text-blue-600 transition-colors duration-300">
+                        {{ task.title }}
+                    </h3>
+                    <p class="text-gray-600 leading-relaxed line-clamp-3">
+                        {{ task.description }}
+                    </p>
+                    <div class="flex justify-end gap-2">
                         <button class="text-sm px-2 py-1 bg-red-100 text-red-600 rounded hover:bg-red-200 transition"
                             @click.stop="deleteTask(task.id)">
                             Deletar
